e0b507431c80525d0b05cef346c99d9864ae3924
Arnoldas Armonas <arnrmn@telesoftas.com>
2018-06-20 14:41:41 +0300

    Provide schedulers from app module

--- mvp/src/main/java/arnrmn/mvp/app/AppModule.kt
+++ mvp/src/main/java/arnrmn/mvp/app/AppModule.kt
@@ -1,6 +1,26 @@
 package arnrmn.mvp.app
 
+import android.content.Context
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
+import dk.tv2.onboarding.IO
+import dk.tv2.onboarding.UI
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 @Module
-abstract class AppModule
+abstract class AppModule {
+    @Binds
+    abstract fun bindApplicationContext(app: MvpApplication): Context
+
+    @Module
+    companion object {
+        @JvmStatic @Provides @IO
+        fun provideIoScheduler(): Scheduler = Schedulers.io()
+
+        @JvmStatic @Provides @UI
+        fun provideUiScheduler(): Scheduler = AndroidSchedulers.mainThread()
+    }
+}

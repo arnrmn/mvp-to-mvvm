5c95917b01a19ebcab2971fca940df322cdac491
Arnoldas Armonas <arnrmn@telesoftas.com>
2018-06-20 15:12:47 +0300

    Bind news list model and presenter

--- mvp/src/main/java/arnrmn/mvp/newslist/fragment/NewsListModule.kt
+++ mvp/src/main/java/arnrmn/mvp/newslist/fragment/NewsListModule.kt
@@ -1,6 +1,14 @@
 package arnrmn.mvp.newslist.fragment
 
+import dagger.Binds
 import dagger.Module
+import dk.tv2.onboarding.FragmentScope
 
 @Module
-abstract class NewsListModule
+abstract class NewsListModule {
+    @Binds @FragmentScope
+    abstract fun bindPresenter(presenter: NewsListPresenter): NewsListContract.Presenter
+
+    @Binds
+    abstract fun bindModel(model: NewsListModel): NewsListContract.Model
+}

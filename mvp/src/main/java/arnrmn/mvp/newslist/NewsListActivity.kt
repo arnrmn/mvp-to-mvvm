a7f3c333f574f63695a5e3b41b396e1da8ad9d2a
Arnoldas Armonas <arnrmn@telesoftas.com>
2018-06-20 15:07:23 +0300

    Setup toolbar in news list activity

--- mvp/src/main/java/arnrmn/mvp/newslist/NewsListActivity.kt
+++ mvp/src/main/java/arnrmn/mvp/newslist/NewsListActivity.kt
@@ -5,12 +5,14 @@ import arnrmn.mvp.newslist.fragment.NewsListFragment
 import arnrmn.mvp.utils.android.BaseActivity
 import arnrmn.mvp.utils.android.replaceFragment
 import arnrmn.mvp_mvvm.R
+import kotlinx.android.synthetic.main.layout_toolbar.*
 
 class NewsListActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_base)
+        setSupportActionBar(toolbar)
         replaceFragment(NewsListFragment.newInstance())
     }
 }

a27c8edb26cde25763ba4b24ba9e7a90a3349f6d
Arnoldas Armonas <arnrmn@telesoftas.com>
2018-07-03 16:17:51 +0300

    Remove reified from LiveData extension

--- mvvm/src/main/java/arnrmn/mvvm/utils/livedata/LiveData.kt
+++ mvvm/src/main/java/arnrmn/mvvm/utils/livedata/LiveData.kt
@@ -4,6 +4,6 @@ import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
 
-inline fun <reified T> LiveData<T>.on(owner: LifecycleOwner, crossinline action: (T) -> Unit) {
+inline fun <T> LiveData<T>.on(owner: LifecycleOwner, crossinline action: (T) -> Unit) {
     this.observe(owner, Observer { data -> data?.let(action) })
 }

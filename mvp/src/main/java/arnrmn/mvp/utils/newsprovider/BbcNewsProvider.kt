5aad8edf8b2a1f1e52f46c9b2224e06c1d54c34a
Arnoldas Armonas <arnrmn@telesoftas.com>
2018-07-04 18:52:31 +0300

    Do not replace fragment on recreation in MVP

--- mvp/src/main/java/arnrmn/mvp/utils/newsprovider/BbcNewsProvider.kt
+++ mvp/src/main/java/arnrmn/mvp/utils/newsprovider/BbcNewsProvider.kt
@@ -2,14 +2,17 @@ package arnrmn.mvp.utils.newsprovider
 
 import arnrmn.mvp.utils.entity.Article
 import arnrmn.mvp.utils.network.RestApi
+import io.reactivex.Completable
 import io.reactivex.Single
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class BbcNewsProvider @Inject constructor(
         private val restApi: RestApi
 ) : NewsProvider {
     override fun getNews(): Single<List<Article>> {
-        return restApi.getTopHeadlines(SOURCE)
+        return Completable.timer(2, TimeUnit.SECONDS)
+                .andThen(restApi.getTopHeadlines (SOURCE))
                 .map { response -> response.articles }
     }
 
